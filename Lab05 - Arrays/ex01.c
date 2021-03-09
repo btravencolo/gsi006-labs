5fea74c887601ea552a5e07358ae48b3c6264e64
travencolo <travencolo@gmail.com>
2021-03-09 18:21:56 -0300

    exercícios 1 ao 8, sem correção automática

---
+++ Lab05 - Arrays/ex01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    printf("<< Listando um vetor >>\n");
+
+    return 0;
+}
+
+/*
+Crie um programa que lê 6 valores inteiros e em seguida mostra na tela os 
+valores lidos. Não utilize loops e utilize vetores para armazenar os números.
+ 
+Exemplo de saída:
+<< Listando um vetor >>
+Entre com o número 1: 5
+Entre com o número 2: 6
+Entre com o número 3: 4
+Entre com o número 4: 0
+Entre com o número 5: 3
+Entre com o número 6: 6
+
+Os valores lidos são: 5 6 4 0 3 6
+*/

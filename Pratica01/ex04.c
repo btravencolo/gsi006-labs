f599bc68aa8689e65eb2d3bddae1a023dde3ce91
Gustavo Borges <ogusborges@gmail.com>
2021-03-27 20:05:00 -0300

    Teste de Commit

---
+++ Pratica01/ex04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("<< Media Aritmetica >>\n");
+    // Insira o seu código aqui.
+}
+
+
+/* 
+Leia e mostre quatro notas, em seguida, calcule e mostre a média aritmética.
+
+Exemplo de Saída:
+----------------------------------------------------------------------------
+<< Media Aritmetica >>
+Digite a 1a nota: 30
+Digite a 2a nota: 40
+Digite a 3a nota: 60
+Digite a 4a nota: 30
+==Notas==
+Nota 1: 30; Nota 2: 40; Nota 3: 60; Nota 4: 30
+Media: 40
+----------------------------------------------------------------------------
+*/

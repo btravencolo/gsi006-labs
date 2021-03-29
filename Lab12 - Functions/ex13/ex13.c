7554a3514bb283e64eabe9ade411b9f95894f0fb
Gustavo Borges <ogusborges@gmail.com>
2021-03-29 19:52:14 -0300

    Update ex13.c

--- Lab12 - Functions/ex13/ex13.c
+++ Lab12 - Functions/ex13/ex13.c
@@ -19,6 +19,12 @@ Cabeçalho: area = calc_area(p1,p2)
 -------------------------------------------------------------------------------
 Exemplo de Saída:
 
-A area do retangulo definido por (1,4) e (4,2) eh 6
+<< Calculo de Area >>
+Digite a coordenada x do ponto 1: 1
+Digite a coordenada y do ponto 1: 4
+Digite a coordenada x do ponto 2: 4
+Digite a coordenada y do ponto 2: 2
+
+A area do retangulo definido por (1.00, 4.00) e (4.00, 2.00) eh 6.00
 -------------------------------------------------------------------------------
 */

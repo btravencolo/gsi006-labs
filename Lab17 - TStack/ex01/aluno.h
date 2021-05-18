dc278e2979f007ba9b320036d8169294b108fd48
travencolo <travencolo@gmail.com>
2021-05-17 22:53:48 -0300

    Exercício pilha

---
+++ Lab17 - TStack/ex01/aluno.h
@@ -0,0 +1,6 @@
+
+struct aluno{
+    int matricula;
+    char nome[30];
+    float n1,n2,n3;
+};
---
+++ Lab17 - TStack/ex01/prog.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "TStack.h"
+
+int main(){
+    struct aluno a;
+    return 0;
+}
